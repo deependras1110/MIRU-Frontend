3678701b39bd3b04651d2c0d2c35909f31d6f10a
Deependra <deependras1110@gmail.com>
2022-11-28 23:19:43 -0600

    Updated the local data

---
+++ miru/src/data/itineraryLocal.js
@@ -0,0 +1,74 @@
+export const itineraryLocal = [
+  {
+    _id: { $oid: "63488939a5d0037229f2c108" },
+    dayTitle: "Flying to Kyoto",
+    date: { $date: { $numberLong: "1689310800000" } },
+    desc: "Go to K's House for accomodation",
+    activities: [],
+    createdAt: { $date: { $numberLong: "1665698105553" } },
+    updatedAt: { $date: { $numberLong: "1665698105553" } },
+    __v: { $numberInt: "0" },
+  },
+  {
+    _id: { $oid: "6348896ba5d0037229f2c10a" },
+    dayTitle: "Welcome Dinner",
+    date: { $date: { $numberLong: "1689483600000" } },
+    desc: "Welcome Dinner",
+    activities: [],
+    createdAt: { $date: { $numberLong: "1665698155809" } },
+    updatedAt: { $date: { $numberLong: "1665698155809" } },
+    __v: { $numberInt: "0" },
+  },
+  {
+    _id: { $oid: "63488a2ea5d0037229f2c10c" },
+    dayTitle: "Explore",
+    date: { $date: { $numberLong: "1689570000000" } },
+    activities: [
+      {
+        name: "Ueno",
+        link: "https://www.google.com/maps/place/Ueno+Station/@35.7058426,139.7898107,15z/data=!4m5!3m4!1s0x60188e9e93d3060b:0x6c89b36d7b4f9244!8m2!3d35.7141672!4d139.7774091",
+        _id: { $oid: "63488a2ea5d0037229f2c10d" },
+      },
+      {
+        name: "Sensoji Temple",
+        link: "https://www.google.com/maps/place/Sens%C5%8D-ji/@35.7038562,139.7988229,14z/data=!4m5!3m4!1s0x60188ec1a4463df1:0x6c0d289a8292810d!8m2!3d35.7147651!4d139.7966553",
+        _id: { $oid: "63488a2ea5d0037229f2c10e" },
+      },
+      {
+        name: "Akhihabara",
+        link: "https://www.google.com/maps/place/Akihabara+Station/@35.7011728,139.7846179,15z/data=!4m5!3m4!1s0x60188ea7e2f93329:0x158f36257ff597b1!8m2!3d35.698383!4d139.7730717",
+        _id: { $oid: "63488a2ea5d0037229f2c10f" },
+      },
+    ],
+    desc: "Ueno,Sensoji Temple, Akihabara",
+    createdAt: { $date: { $numberLong: "1665698350970" } },
+    updatedAt: { $date: { $numberLong: "1665698350970" } },
+    __v: { $numberInt: "0" },
+  },
+  {
+    _id: { $oid: "63488c9e326887521f145bac" },
+    date: { $date: { $numberLong: "1689656400000" } },
+    dayTitle: "Exploree",
+    activities: [
+      {
+        name: "Tokyo Station",
+        link: "https://www.google.com/maps/place/Ueno+Station/@35.7058426,139.7898107,15z/data=!4m5!3m4!1s0x60188e9e93d3060b:0x6c89b36d7b4f9244!8m2!3d35.7141672!4d139.7774091",
+        _id: { $oid: "63488c9e326887521f145bad" },
+      },
+      {
+        name: "Imperial Palace",
+        link: "https://www.google.com/maps/place/Sens%C5%8D-ji/@35.7038562,139.7988229,14z/data=!4m5!3m4!1s0x60188ec1a4463df1:0x6c0d289a8292810d!8m2!3d35.7147651!4d139.7966553",
+        _id: { $oid: "63488c9e326887521f145bae" },
+      },
+      {
+        name: "Sengakuji Temple",
+        link: "https://www.google.com/maps/place/Akihabara+Station/@35.7011728,139.7846179,15z/data=!4m5!3m4!1s0x60188ea7e2f93329:0x158f36257ff597b1!8m2!3d35.698383!4d139.7730717",
+        _id: { $oid: "63488c9e326887521f145baf" },
+      },
+    ],
+    desc: "Tokyo Station,Imperial Palace, Sengakuji Temple",
+    createdAt: { $date: { $numberLong: "1665698974614" } },
+    updatedAt: { $date: { $numberLong: "1665698974614" } },
+    __v: { $numberInt: "0" },
+  },
+];
